4506bb72c650c2b88e503187964fb4090991a1ec
Savio-Anjos <savioanjos2020@gmail.com>
2023-12-04 22:41:52 -0300

    feat :sparkles: Create initial structure

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import fastify from "fastify";
+import { ZodError } from "zod";
+
+export const app = fastify();
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "Validation error.", issues: error.format() });
+  }
+
+  return reply.status(500).send({ message: "Internal server error." });
+});
