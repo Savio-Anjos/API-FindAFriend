e6f471bf40e89da2cf1198019c3c79028d30ffb7
Savio-Anjos <savioanjos2020@gmail.com>
2023-12-06 10:14:03 -0300

    test :white_check_mark: Organization use case
    Added unit tests for creating the organization

---
+++ src/use-cases/create-organization.spec.ts
@@ -0,0 +1,45 @@
+import { compare } from "bcryptjs";
+import { InMemoryOrganizationRepository } from "../repositories/in-memory/in-memory-organization-repository";
+import { OrganizationUseCase } from "./create-organization";
+import { beforeEach, describe, expect, it } from "vitest";
+
+let organizationRepository: InMemoryOrganizationRepository;
+let sut: OrganizationUseCase;
+
+describe("Create Organization Use Case", () => {
+  beforeEach(() => {
+    organizationRepository = new InMemoryOrganizationRepository();
+    sut = new OrganizationUseCase(organizationRepository);
+  });
+
+  it("should be able to create organization", async () => {
+    const { organization } = await sut.execute({
+      name: "Organization Test",
+      email: "johndoe@example.com",
+      password: "123456",
+      cep: " 45345000",
+      address: "Rua test",
+      whatsapp: "73 99999999",
+    });
+
+    expect(organization.id).toEqual(expect.any(String));
+  });
+
+  it("should create a hash of the password", async () => {
+    const { organization } = await sut.execute({
+      name: "Organization Test",
+      email: "johndoe@example.com",
+      password: "123456",
+      cep: " 45345000",
+      address: "Rua test",
+      whatsapp: "73 99999999",
+    });
+
+    const isPasswordCorrectlyHashed = await compare(
+      "123456",
+      organization.password_hash
+    );
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
+});
