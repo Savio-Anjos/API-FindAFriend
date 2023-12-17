7429013b358d9dee0f790434960dd7651e083a92
Savio-Anjos <savioanjos2020@gmail.com>
2023-12-17 13:18:48 -0300

    feat :sparkles: Added middleware verify-jwt

--- src/http/controllers/pets/routes.ts
+++ src/http/controllers/pets/routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { createPet } from "./create-pet";
+import { verifyJWT } from "@/http/middlewares/verify-jwt";
 
 export async function petsRoutes(app: FastifyInstance) {
-  app.post("/pets", createPet);
+  app.post("/pets", { onRequest: [verifyJWT] }, createPet);
 }
