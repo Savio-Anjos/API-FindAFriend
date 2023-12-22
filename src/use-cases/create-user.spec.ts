de533086f1bedc1a1a3f60c235bb1cf7b66f0165
Savio-Anjos <savioanjos2020@gmail.com>
2023-12-21 23:01:20 -0300

    test :white_check_mark: Users use case
    Added unit tests for creating the user

---
+++ src/use-cases/create-user.spec.ts
@@ -0,0 +1,40 @@
+import { UsersRepository } from "@/repositories/users-repository";
+import { CreateUserUseCase } from "./create-user";
+import { beforeEach, describe, expect, it } from "vitest";
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+import { compare } from "bcryptjs";
+
+let usersRepository: UsersRepository;
+let sut: CreateUserUseCase;
+
+describe("Create User Use Case", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    sut = new CreateUserUseCase(usersRepository);
+  });
+
+  it("should be able to create user", async () => {
+    const { user } = await sut.execute({
+      name: "John Doe",
+      email: "jonhdoe@example.com",
+      password: "123456",
+    });
+
+    expect(user.id).toEqual(expect.any(String));
+  });
+
+  it("should reate a hash of the password", async () => {
+    const { user } = await sut.execute({
+      name: "John Doe",
+      email: "jonhdoe@example.com",
+      password: "123456",
+    });
+
+    const isPasswordCorrectlyHashed = await compare(
+      "123456",
+      user.password_hash
+    );
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
+});
