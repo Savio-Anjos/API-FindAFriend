ec502fedbd94fb2f5c848f6a757e59be2b752bf2
Savio-Anjos <savioanjos2020@gmail.com>
2023-12-21 22:06:13 -0300

    feat :sparkles: Added model user

---
+++ prisma/migrations/20231222010341_added_model_user/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
